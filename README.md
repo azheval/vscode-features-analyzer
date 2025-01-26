b6b004e3bc5e8d90a93536ff8229189353db2dba
Valentin Azheronok <azheval@gmail.com>
2025-01-26 04:47:06 +0300

    feat: Добавлена проверка начальных отступов

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # Features Analyser
 
-Команды:
+## Команды
 
 - FeaturesAnalyzer.Create Features List. Сканирует каталог из `featuresAnalyzer.featuresFolder` и строит таблицу по данным фича-файлов.
 
 ![out table](images/image_1.png)
+
+- FeaturesAnalyser.Check Indentation. Сканирует каталог из `featuresAnalyzer.featuresFolder` и строит таблицу по некорректным отступам.
+
+![out table](images/image_2.png)
+
+## Настройка
+
+- featuresAnalyzer.featuresFolder. Каталог для сканирования.
+- featuresAnalyzer.defaultIndentation. Поддерживаемые варианты значения, кроме незаполненного: `space`, `tab`.
+
+## Описание
+
+### Check Indentation
+
+- Проверяет наличие пробелов и табуляций в начальных отступах в одной строке.
+
+![out table](images/image_3.png)
+
+- Проверяет наличие пробелов и табуляций в начальных отступах в файле при наличии тега `@tree`.
+
+![out table](images/image_4.png)
+
+- Проверяет наличие пробелов в начальных отступах в файле при значении `tab` настройки `featuresAnalyzer.defaultIndentation`.
+
+![out table](images/image_5.png)
+
+- Проверяет наличие табуляции в начальных отступах в файле при значении `space` настройки `featuresAnalyzer.defaultIndentation`.
+
+![out table](images/image_6.png)
--- src/extension.js
+++ src/extension.js
@@ -4,24 +4,34 @@ const path = require("path");
 
 let allFilesInfo = [];
 let panel = null;
+let defaultIndentation = "";
 
 function activate(context) {
-    let disposable = vscode.commands.registerCommand("featuresAnalyzer.createFeaturesList", async function () {
-        const workspaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath;
-        const scanDirectory = vscode.workspace.getConfiguration('featuresAnalyzer', vscode.Uri.file(workspaceFolder)).get("featuresFolder");
-        
-        if (scanDirectory) {
-            allFilesInfo = [];
-            analyzeDirectory(scanDirectory);
-        } else {
-            vscode.window.showInformationMessage("No features directory selected");
-        }
+    const workspaceFolder = vscode.workspace.workspaceFolders[0].uri.fsPath;
+    const config = vscode.workspace.getConfiguration('featuresAnalyzer');
+    const scanDirectory = config.get("featuresFolder");
+    defaultIndentation = config.get('defaultIndentation');
+
+    if (!scanDirectory) {
+        vscode.window.showInformationMessage("No features directory selected");
+        return;
+    }
+
+    let commandCreateFeaturesList = vscode.commands.registerCommand("featuresAnalyzer.createFeaturesList", async function () {
+        allFilesInfo = [];
+        analyzeDirectory(scanDirectory, analyzeFileForFeaturesList);
     });
 
-    context.subscriptions.push(disposable);
+    let commandCheckIndentation = vscode.commands.registerCommand('featuresAnalyzer.checkIndentation', async function () {
+        allFilesInfo = [];
+        analyzeDirectory(scanDirectory, analyzeCheckIndentation);
+    });
+
+    context.subscriptions.push(commandCreateFeaturesList);
+    context.subscriptions.push(commandCheckIndentation);
 }
 
-function analyzeDirectory(dir) {
+function analyzeDirectory(dir, analyzeFileCallback) {
     console.log(`Analyzing directory: ${dir}`);
     fs.readdir(dir, (err, files) => {
         if (err) {
@@ -38,16 +48,16 @@ function analyzeDirectory(dir) {
                 }
 
                 if (stats.isDirectory()) {
-                    analyzeDirectory(filePath);
+                    analyzeDirectory(filePath, analyzeFileCallback);
                 } else if (path.extname(file) === ".feature") {
-                    analyzeFile(filePath);
+                    analyzeFileCallback(filePath);
                 }
             });
         });
     });
 }
 
-function analyzeFile(filePath) {
+function analyzeFileForFeaturesList(filePath) {
     console.log(`Analyzing file: ${filePath}`);
     fs.readFile(filePath, "utf8", (err, data) => {
         if (err) {
@@ -82,11 +92,11 @@ function analyzeFile(filePath) {
         }
 
         allFilesInfo.push(fileInfo);
-        displayAllFilesInfo();
+        displayAnalyzeFileForFeaturesList();
     });
 }
 
-function displayAllFilesInfo() {
+function displayAnalyzeFileForFeaturesList() {
     console.log(`Displaying info for all files`);
 
     if (!panel) {
@@ -148,6 +158,170 @@ function displayAllFilesInfo() {
                         <th>Export</th>
                         <th>Author</th>
                         <th>Scenario</th>
+                    </tr>
+                    ${tableRows}
+                </table>
+                <script>
+                    window.onload = function() {
+                        console.log('DOM fully loaded and parsed');
+                        const input = document.getElementById('searchInput');
+                        input.addEventListener('keyup', function() {
+                            console.log('Filtering table');
+                            const filter = input.value.toLowerCase();
+                            const table = document.getElementById('featureTable');
+                            const tr = table.getElementsByTagName('tr');
+
+                            for (let i = 1; i < tr.length; i++) {
+                                const td = tr[i].getElementsByTagName('td');
+                                let showRow = false;
+                                for (let j = 0; j < td.length; j++) {
+                                    if (td[j]) {
+                                        const txtValue = td[j].textContent || td[j].innerText;
+                                    if (txtValue.toLowerCase().indexOf(filter) > -1) {
+                                        showRow = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            tr[i].style.display = showRow ? '' : 'none';
+                            }
+                        });
+                    };
+
+                    function openFile(filePath) {
+                        console.log('Opening file:', filePath);
+                        const vscode = acquireVsCodeApi();
+                        vscode.postMessage({
+                            command: 'openFile',
+                            filePath: filePath
+                        });
+                    }
+                </script>
+            </body>
+        </html>
+    `;
+
+    console.log(`Panel content set for all files`);
+}
+
+function analyzeCheckIndentation(filePath) {
+    console.log(`Analyzing file: ${filePath}`);
+    fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) {
+            console.error(`Error reading file ${filePath}:`, err);
+            return;
+        }
+
+        console.log(`File read successfully: ${filePath}`);
+        const lines = data.split('\n');
+        const fileInfo = {
+            fileName: path.basename(filePath),
+            relativePath: vscode.workspace.asRelativePath(filePath),
+            inconsistentLines: [],
+        };
+
+        let hasTagTree = false;
+        let hasSpaces = false;
+        let hasTabs = false;
+        let ckeckSpaces = false;
+        let ckeckTabs = false;
+
+        if (defaultIndentation === 'tab') {
+            ckeckSpaces = true;
+        } else if (defaultIndentation === 'space') {
+            ckeckTabs = true;
+        }
+
+        lines.forEach((line, index) => {
+            const leadingWhitespace = line.match(/^\s*/)[0];
+            if (leadingWhitespace.includes(' ')) {
+                hasSpaces = true;
+            }
+            if (leadingWhitespace.includes('\t')) {
+                hasTabs = true;
+            }
+            if (leadingWhitespace.includes(' ') && leadingWhitespace.includes('\t')) {
+                fileInfo.inconsistentLines.push(`in line ${index + 1} detected spaces and tabs`);
+            }
+            const lowerCaseLine = line.toLowerCase();
+            if (lowerCaseLine.includes("@tree")) {
+                hasTagTree = true;
+            }
+        });
+
+        if (hasTagTree && hasSpaces && hasTabs) {
+            fileInfo.inconsistentLines.push('tag tree detected, in file lines detected spaces and tabs');
+        }
+
+        if (ckeckSpaces && hasSpaces) {
+            fileInfo.inconsistentLines.push('inconsistent spaces detected in file lines, tabs expected');
+        }
+
+        if (ckeckTabs && hasTabs) {
+            fileInfo.inconsistentLines.push('inconsistent tabs detected in file lines, spaces expected');
+        }
+
+        allFilesInfo.push(fileInfo);
+        displayAnalyzeCheckIndentation();
+    });
+}
+
+function displayAnalyzeCheckIndentation() {
+    console.log(`Displaying info for all files`);
+
+    if (!panel) {
+        panel = vscode.window.createWebviewPanel("inconsistentFeaturesList", "Inconsistent Features List", vscode.ViewColumn.One, {
+            enableScripts: true
+        });
+        console.log('Webview panel created');
+
+        panel.webview.onDidReceiveMessage(message => {
+            switch (message.command) {
+                case 'openFile':
+                try {
+                    const filePath = path.join(vscode.workspace.workspaceFolders[0].uri.fsPath, message.filePath);
+                    vscode.workspace.openTextDocument(filePath).then(doc => {
+                        vscode.window.showTextDocument(doc);
+                    });
+                } catch (error) {
+                    console.error(`Error opening file ${message.filePath}:`, error);
+                }
+                break;
+            }
+        });
+    }
+
+    const tableRows = allFilesInfo.map(fileInfo => {
+        return fileInfo.inconsistentLines.map(inconsistentLine => {
+            return `
+                <tr>
+                    <td><a href="#" onclick="openFile('${fileInfo.relativePath.replace(/\\/g, '\\\\')}')">${fileInfo.fileName}</a></td>
+                    <td>${fileInfo.relativePath}</td>
+                    <td>${inconsistentLine}</td>
+                </tr>`;
+        }).join("");
+    }).join("");
+
+    panel.webview.html = `
+        <html>
+            <head>
+                <style>
+                    #searchInput {
+                        margin-bottom: 10px;
+                    }
+                    .center {
+                        text-align: center;
+                    }
+                </style>
+            </head>
+            <body>
+                <h2>Inconsistent Features List</h2>
+                <input type="text" id="searchInput" placeholder="Search for features..">
+                <table id="featureTable" border="1">
+                    <tr>
+                        <th>File Name</th>
+                        <th>Path</th>
+                        <th>Line</th>
                     </tr>
                     ${tableRows}
                 </table>
